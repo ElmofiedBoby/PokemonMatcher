71404cb5430ccc67d984fd076970dca94cecdc90
Nithin Joseph <nithjoe1@gmail.com>
2022-12-14 09:22:03 -0500

    portnumber listen

--- pokemonFinderServer.js
+++ pokemonFinderServer.js
@@ -312,8 +312,9 @@ function randomStr(len, arr) {
 }
 
 /* Server Console Logic */
-var dataInput = '';
-app.listen(portNumber);
+//var dataInput = '';
+app.listen(portNumber, () => console.log("Server is running..."));
+/*
 console.log(`Web server started and running at http://localhost:${portNumber}`);
 process.stdin.setEncoding("utf8");
 process.stdout.write("^_^: ")
@@ -347,4 +348,4 @@ process.stdin.on('readable', () => {
 		}
 		process.stdout.write("^_^: ");
 	}
-});
+});*/
